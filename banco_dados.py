b982e3349f47a0d5727c41bd0d5d1d72ae7bdaf9
AliceOlivv <aliceolivf@gmail.com>
2025-05-28 09:29:04 -0300

    Atualização do banco de dados

--- banco_dados.py
+++ banco_dados.py
@@ -1,9 +1,9 @@
 import sqlite3 as sql
 
-banco = sql.connect('banco_lojaArcanjo')
+banco = sql.connect('banco_lojaArcanjo.db')
 
 cursor = banco.cursor()
-#cursor.execute("CREATE TABLE loja (Nome_produto text, valor real, estoque integer, codigo integer)")Pneu 
+#cursor.execute("CREATE TABLE loja (Nome_produto text, valor real, estoque integer, codigo integer)")
 #cursor.execute("INSERT INTO loja VALUES('Pneu traseiro biz',180.00, 5, 1)")
 
 nome = input("Digite o nome do produto: ")
