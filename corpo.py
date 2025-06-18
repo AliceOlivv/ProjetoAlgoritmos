87d091178bb0dd2fd62b6cd04f77d305eeb5f271
laysiara <laysiara569@gmail.com>
2025-06-18 16:20:25 -0300

    Organização das pastas: corpo e funções

---
+++ corpo.py
@@ -0,0 +1,90 @@
+
+import inquirer
+
+import funções
+
+pergunta = [
+    inquirer.List(
+        'inicio',
+        message="O que deseja fazer?",
+        choices=[
+            'Pesquisa de Produto ou Agendamento de serviço',
+            'Cadastramento de novos produtos ou serviços',
+            'Gerenciamento de Estoque',
+            'Resumo Mensal'
+        ]
+    )
+]
+
+resposta = inquirer.prompt(pergunta)
+
+if resposta["inicio"]== "Pesquisa de Produto ou Agendamento de serviço":
+        
+    prod_ou_serv = [
+        inquirer.List(
+            'escolha',
+            message="O que deseja fazer?",
+            choices=[
+                'Pesquisa de Produto',
+                'Agendamento de Serviço',
+            ]
+        )
+    ]
+
+    resposta2=inquirer.prompt(prod_ou_serv)
+
+    if resposta2["escolha"]== "Pesquisa de Produto":
+
+        funções.PesquisaDeProdutos()
+
+    #elif resposta2["escolha"]== "Agendamento de serviço"
+             
+elif resposta["inicio"]== "Cadastramento de novos produtos ou serviços":
+      
+    cadastramento = [
+         inquirer.List(
+            'cadastro',
+            message="Deseja fazer um novo cadastramento de qual setor?",
+            choices=[
+                'Produtos',
+                'Serviços',
+            ]
+        )
+    ]
+
+    resposta3=inquirer.prompt(cadastramento)
+
+    if resposta3["cadastro"]=="Produtos":
+        funções.CadastrarProdutos()
+
+    if resposta3["cadastro"]=="Serviços":
+        funções.CadastrarServiço()
+         
+elif resposta["inicio"]== "Gerenciamento de Estoque":
+
+    gerenciamento = [
+         inquirer.List(
+            'estoque',
+            message="O que deseja fazer ?",
+            choices=[
+                'Adicionar produto',
+                'Remover produto',
+            ]
+        )
+    ] 
+
+    resposta4=inquirer.prompt(gerenciamento)
+
+    if resposta4["estoque"]==" Adicionar produto":
+         funções.AdicionarProdutoAoEstoque()
+    if resposta4["estoque"]==" Remover produto":
+         funções.RemoverProdutoAoEstoque()
+
+#if resposta["inicio"]== "Resumo Mensal":
+         
+
+      
+      
+
+            
+input("")
--- funções.py
+++ funções.py
@@ -1,66 +1,84 @@
 #import sqlite3
 
-
 #banco = sqlite3.connect("banco_lojaArcanjo.db")
-
 
 #cursor = banco.cursor()
 
-#import inquirer
+#banco.close()
+
+#(Usar esses comandos toda vez q fizer uma função usando o banco)
+
+import inquirer
 
 
-def AdicionarServiço(nome,valor,codigo_serviço):
+def CadastrarServiço():
+    import sqlite3
+
+
+    banco = sqlite3.connect("banco_lojaArcanjo.db")
+
+
+    cursor = banco.cursor()
+
+    serviço=input("Digite o nome do serviço")
+
+    valor_serviço=("Digite o valor do serviço: ")
+    while not valor_serviço.isnumeric():
+            print("Erro. Digite um número:")
+            valor_serviço=(input(""))
+    valor_serviço=float(valor_serviço)
+                                                         
+    codigo_serviço=("Digite o código do serviço: ")
+    while not codigo_serviço.isnumeric():
+            print("Erro. Digite um número inteiro:")
+            codigo_serviço=(input(""))
+    codigo_serviço=int(codigo_serviço)
     
-    cursor.execute("INSERT INTO serviço VALUES (?, ?, ?)", (serviço, valor_serviço, codigo_serviço))
+    cursor.execute("INSERT INTO serviços VALUES (?, ?, ?)", (serviço, valor_serviço, codigo_serviço))
     banco.commit()
-    return print("Adicionado")
+    banco.close()
+    print("Adicionado")
 
-def AdicionarEstoquedeProduto()
-    
+#def CadastrarProdutos()
 
-pergunta = [
-    inquirer.List(
-        'acao',
-        message="O que deseja fazer?",
-        choices=[
-            'Pesquisa de Produto',
-            'Serviços disponíveis',
-            'Cadastro de produtos e serviços',
-            'Gerenciamento de Estoque',
-            'Lucro'
-        ]
-    )
-]
+def PesquisaDeProdutos():
+    import sqlite3
 
-resposta = inquirer.prompt(pergunta)
 
-if resposta["acao"]== "Pesquisa de Produto":
+    banco = sqlite3.connect("banco_lojaArcanjo.db")
+
+
+    cursor = banco.cursor()
 
     codigo=(input("Escreva o código do produto que deseja pesquisar: "))
     while not codigo.isnumeric():
-        print("Erro.Digite um número:")
+        print("Erro. Digite um número:")
         codigo=(input(""))
 
     codigo=int(codigo)
-    
 
     cursor.execute("SELECT * FROM loja WHERE codigo== ? ",(codigo,))
     dados= cursor.fetchall()
+    banco.close()
 
     if dados == []:
-            print("Não existe dados para esse código")
-            print("Deseja")
+        print("Não existe dados para esse código")
     else:
-
         for linha in dados:
             print("Nome:",linha[0])
             print("valor:", linha[1])
             print("Quantidade de itens:", linha[2])
 
-input("")
+#def AdicionarProdutoAoEstoque()
+     
+#def RemoverProdutoAoEstoque()
+
+     
 
     
-        
+
+
+
 
     
 
